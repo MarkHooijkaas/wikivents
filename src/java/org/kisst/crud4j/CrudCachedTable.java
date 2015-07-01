198772de73a1a8dc10c0061cc76354bec80d6667
Mark Hooijkaas <mark@kisst.org>
2015-07-02 00:55:03 +0200

    added @Override's

--- src/java/org/kisst/crud4j/CrudCachedTable.java
+++ src/java/org/kisst/crud4j/CrudCachedTable.java
@@ -8,11 +8,11 @@ public abstract class CrudCachedTable<T extends CrudObject> extends CrudTable<T>
 	private final ConcurrentHashMap<String, T> map=new ConcurrentHashMap<String,T>();
 	public CrudCachedTable(Storage<T> storage) { super(storage);	}
 	abstract protected T createObject(Struct  props);
-	public void create(T doc) {
+	@Override public void create(T doc) {
 		super.create(doc);
 		map.put(doc._id, doc);
 	}
-	public T read(String key) { 
+	@Override public T read(String key) { 
 		T result = map.get(key);
 		if (result==null) {
 			result=super.read(key);
@@ -20,11 +20,11 @@ public abstract class CrudCachedTable<T extends CrudObject> extends CrudTable<T>
 		}
 		return result;
 	}
-	public void update(T oldValue, T newValue) { 
+	@Override public void update(T oldValue, T newValue) { 
 		super.update(oldValue, newValue);
 		map.put(newValue._id, newValue);
 	}
-	public void delete(T oldValue) {
+	@Override public void delete(T oldValue) {
 		super.delete(oldValue);
 		map.remove(oldValue._id);
 	}
