b6f225ced8a4aa6197c8e2c4ff1218b1d3e809d8
Mark Hooijkaas <mark@kisst.org>
2015-07-12 07:07:19 +0200

    some cleanup of build and test code

--- src/test/java/test/Main.java
+++ src/test/java/test/Main.java
@@ -1,7 +1,8 @@
 package test;
 
-import java.io.File;
-
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.kisst.item4j.struct.HashStruct;
 
 import club.wikivents.model.User;
@@ -13,36 +14,52 @@ import com.mongodb.MongoClient;
 
 public class Main {
 	public static void main(String[] args) {
+		BasicConfigurator.configure();
+		Logger.getLogger("org.mongodb").setLevel(Level.WARN);
 
-		WikiventsModel model = WikiventsModels.createFileModel(new File("test/data"));
-		//WikiventsModel model = mongoModel();
+		debug("started");
+
+		WikiventsModel model = null;
+		if (args.length>0 && args[0].equals("mongo"))
+			model = mongoModel();
+		else
+			model=WikiventsModels.createFileModel(new java.io.File("test/data"));
+		debug("created model");
 
 		for (User u: model.users.findAll())
-			System.out.println("***"+u);
+			System.out.println("\t"+u);
+		debug("fetched users");
 		
 		HashStruct doc=new HashStruct();
 		User.schema.username.setValue(doc, "Mark1967");
 		User.schema.email.setValue(doc, "mark@wikivents.nl");
 		User.schema.password.setValue(doc, "secret");
 		//model.users().create(new User(doc));
-		System.out.println("Added user");
-		//model.users().create(new User(doc));
+		model.users.create(new User(doc));
+		debug("Added user");
 
 		User.schema.username.setValue(doc, "PKO");
 		User.schema.email.setValue(doc, "pko@wikivents.nl");
 		User.schema.password.setValue(doc, "secret");
-		//model.users().create(new User(doc));
-		System.out.println("Added user");
+		model.users.create(new User(doc));
+		debug("Added user");
 
 		if (mongoClient!=null)
 			mongoClient.close();
+		debug("done");
 	}
 
+	private static long stopwatch = System.currentTimeMillis();
+	private static void debug(String str) {		
+		System.out.println((System.currentTimeMillis()-stopwatch)+"\t"+str);
+		stopwatch = System.currentTimeMillis();
+	}
 	
 	static MongoClient mongoClient = null;
 	public static WikiventsModel mongoModel() {
 		mongoClient = new MongoClient("localhost");
 		DB db = new DB(mongoClient,"wikivents");
+		debug("connected to db "+db.getName());
 		return WikiventsModels.createMongoModel(db);
 	}
 }
