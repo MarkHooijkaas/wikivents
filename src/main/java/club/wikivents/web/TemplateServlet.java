bfd1a8b4763f98cbbd9f1c9a23a2df15a5398db6
Mark Hooijkaas <mark@kisst.org>
2015-07-12 21:56:17 +0200

    started to use mustache templating

--- src/main/java/club/wikivents/web/TemplateServlet.java
+++ src/main/java/club/wikivents/web/TemplateServlet.java
@@ -1,5 +1,6 @@
 package club.wikivents.web;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.HashMap;
@@ -9,33 +10,37 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.kisst.props4j.Props;
 
-import freemarker.template.Configuration;
-import freemarker.template.DefaultObjectWrapper;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+
 
 public class TemplateServlet extends BaseServlet {
-	private static final Configuration cfg= new Configuration();;
-	static {
-		//cfg.setDirectoryForTemplateLoading(new File("src/java"));
-		cfg.setClassForTemplateLoading(TemplateServlet.class, "/");
-		cfg.setObjectWrapper(new DefaultObjectWrapper());
-	}
+	//private static final Configuration cfg= new Configuration();;
+	//static {
+	//	//cfg.setDirectoryForTemplateLoading(new File("src/java"));
+	//	cfg.setClassForTemplateLoading(TemplateServlet.class, "/");
+	//	cfg.setObjectWrapper(new DefaultObjectWrapper());
+	//}
 
 
 	private final String templateName;
+	private final Mustache template;
+    private final MustacheFactory factory = new DefaultMustacheFactory(new File("src/templates"));
+	private boolean debugMode=true;
 
 	public TemplateServlet(Props props) {
 		super(props);
-		this.templateName=this.getClass().getName().replace('.', '/')+".template";
+		this.templateName=this.getClass().getSimpleName().replace('.', '/')+".template";
+	    this.template = factory.compile(templateName);
 	}
 
 	public TemplateServlet(Props props, String templateName) {
 		super(props);
 		this.templateName=templateName;
+	    this.template = factory.compile(templateName);
 	}
 
-	
 	public void handle(HttpServletRequest request, HttpServletResponse response) {
 		if (getUser(request, response)==null)
 			return;
@@ -43,13 +48,14 @@ public class TemplateServlet extends BaseServlet {
 			HashMap<String, Object> root=new HashMap<String, Object>();
 			//root.put("model", model);
 			addContext(root);
-
-			Template temp = cfg.getTemplate(templateName);
+			Mustache templ = this.template;
+			if (debugMode)
+				templ=new DefaultMustacheFactory(new File("src/templates")).compile(templateName);
+			
 			Writer out = response.getWriter();
-			temp.process(root, out);
+		    templ.execute(out, root);	
 			out.flush();
 		}
-		catch (TemplateException e) { throw new RuntimeException(e); } 
 		catch (IOException e)  { throw new RuntimeException(e); }
 	}
 
