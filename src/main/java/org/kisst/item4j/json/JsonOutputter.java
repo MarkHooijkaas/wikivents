6f54c5c7eb9af3903908ea6f0180f5ca06a116ba
Mark Hooijkaas <mark@hooijkaas.net>
2015-07-28 12:39:24 +0200

    added support for sequences

--- src/main/java/org/kisst/item4j/json/JsonOutputter.java
+++ src/main/java/org/kisst/item4j/json/JsonOutputter.java
@@ -3,16 +3,17 @@ package org.kisst.item4j.json;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.kisst.item4j.Item;
+import org.kisst.item4j.seq.ItemSequence;
 import org.kisst.item4j.struct.Struct;
 import org.kisst.util.StringUtil;
 
 public class JsonOutputter {
-	private final String indentString;
-	public JsonOutputter(String indent) { this.indentString=indent; }
+	//private final String indentString;
+	public JsonOutputter(String indent) { }//this.indentString=indent; }
 
 	public String createString(Struct struct) {
 		StringWriter out=new StringWriter();
-		out.write("{");
 		write(new PrintWriter(out), struct);
 		return out.toString();
 	}
@@ -20,35 +21,49 @@ public class JsonOutputter {
 	public void write(PrintWriter out, Struct struct) { write(out,struct,""); }
 	public void write(PrintWriter out, Struct struct, String currentIndent) {
 		boolean firstElement=true;
+		out.write('{');
 		for (String name: struct.fieldNames()) {
 			Object value=struct.getObject(name,null);
 			if (value==null)
 				continue;
-			if (! firstElement) {
-				if (indentString==null)
-					out.write(", ");
-				else
-					out.write(",\n");
-			}
+			if (! firstElement)
+				out.write(", ");
 			firstElement=false;
 			out.print('"'+name+"\":");
-			if (value instanceof Struct) {
-				if (indentString==null)
-					write(out, (Struct) value, currentIndent);
-				else {
-					//out.write(" ");
-					write(out, (Struct) value, currentIndent+indentString);
-
-				}
-			}
-			else {
-				if (value instanceof Number)
-					out.write(value.toString());
-				else
-					out.write(StringUtil.doubleQuotedString(value.toString()));
-			}
+			write(out,value);
 		}
 		out.write('}');
 	}
+	public void write(PrintWriter out, ItemSequence seq) {
+		out.write('[');
+		System.out.println("seq size-"+seq.size()+" value="+seq);
+		boolean firstElement=true;
+		for (int i=0; i<seq.size(); i++) {
+			Item value=seq.getItem(i);
+			if (! firstElement)
+				out.write(", ");
+			
+			firstElement=false;
+			write(out,value);
+		}
+		out.write(']');
+	}
 
+	
+	public void write(PrintWriter out, Object value) {
+		if (value==null)
+			return;
+		if (value instanceof Item)
+			value=((Item) value).asObject();
+		if (value instanceof Struct)
+			write(out,(Struct) value);
+		else if (value instanceof ItemSequence)
+			write(out,(ItemSequence) value);
+		else {
+			if (value instanceof Number)
+				out.write(value.toString());
+			else
+				out.write(StringUtil.doubleQuotedString(value.toString()));
+		}
+	}
 }
