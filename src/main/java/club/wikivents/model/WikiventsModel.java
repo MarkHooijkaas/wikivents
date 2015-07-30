df9e2e0873d403d7024b878c6393a0a3d3bb42f9
Mark Hooijkaas <mark@hooijkaas.net>
2015-07-30 13:43:54 +0200

    user the usernameIndex for loginPage, and remove findUserByUsername method

--- src/main/java/club/wikivents/model/WikiventsModel.java
+++ src/main/java/club/wikivents/model/WikiventsModel.java
@@ -3,7 +3,6 @@ package club.wikivents.model;
 import org.kisst.crud4j.CrudModel;
 import org.kisst.crud4j.CrudTable;
 import org.kisst.crud4j.StorageOption;
-import org.kisst.item4j.struct.Struct;
 
 
 public class WikiventsModel extends CrudModel {
@@ -15,14 +14,5 @@ public class WikiventsModel extends CrudModel {
 
 	public final UniqueIndex<User> usernameIndex = getUniqueIndex(User.class, User.schema.username);
 	public final UniqueIndex<User> emailIndex    = getUniqueIndex(User.class, User.schema.email);
-
-	
-	public User findUsername(String username) {
-		for (Struct u: users.findAll()) { 
-			if (u.getString(username).equals(username)) // || u.email.equals(username))
-				return new User(this,u);
-		}
-		return null;
-	}
 
 }
