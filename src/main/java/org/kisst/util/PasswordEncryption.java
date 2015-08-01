18d4cba4462065c026afb1e70db02afe2f961a3b
Mark Hooijkaas <mark@hooijkaas.net>
2015-08-01 21:34:38 +0200

    some new classes for the previous commit

---
+++ src/main/java/org/kisst/util/PasswordEncryption.java
@@ -0,0 +1,85 @@
+// From http://blog.jerryorr.com/2012/05/secure-password-storage-lots-of-donts.html
+package org.kisst.util;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.xml.bind.DatatypeConverter;
+
+public class PasswordEncryption {
+	public static boolean authenticate(String attemptedPassword, String encryptedPassword, String salt) {
+		try {
+			return authenticate(attemptedPassword, toByteArray(encryptedPassword), toByteArray(salt));
+		}
+		catch (NoSuchAlgorithmException e) { throw new RuntimeException(e);} 
+		catch (InvalidKeySpecException e) { throw new RuntimeException(e);}
+	}
+
+	public static String encryptPassword(String password, String salt) {
+		try {
+			return toHexString(getEncryptedPassword(password, toByteArray(salt)));
+		}
+		catch (NoSuchAlgorithmException e) { throw new RuntimeException(e);} 
+		catch (InvalidKeySpecException e) { throw new RuntimeException(e);}
+	}
+
+	public static String createSaltString() { 
+		try {
+			return toHexString(generateSalt());
+		}
+		catch (NoSuchAlgorithmException e) { throw new RuntimeException(e);}
+	}
+
+	
+	
+	public static String toHexString(byte[] array) { return DatatypeConverter.printHexBinary(array); }
+	public static byte[] toByteArray(String s) { return DatatypeConverter.parseHexBinary(s); }
+	
+	public static boolean authenticate(String attemptedPassword, byte[] encryptedPassword, byte[] salt)
+			throws NoSuchAlgorithmException, InvalidKeySpecException {
+		// Encrypt the clear-text password using the same salt that was used to
+		// encrypt the original password
+		byte[] encryptedAttemptedPassword = getEncryptedPassword(attemptedPassword, salt);
+
+		// Authentication succeeds if encrypted password that the user entered
+		// is equal to the stored hash
+		return Arrays.equals(encryptedPassword, encryptedAttemptedPassword);
+	}
+
+	public static byte[] getEncryptedPassword(String password, byte[] salt)
+			throws NoSuchAlgorithmException, InvalidKeySpecException {
+		// PBKDF2 with SHA-1 as the hashing algorithm. Note that the NIST
+		// specifically names SHA-1 as an acceptable hashing algorithm for PBKDF2
+		String algorithm = "PBKDF2WithHmacSHA1";
+		// SHA-1 generates 160 bit hashes, so that's what makes sense here
+		int derivedKeyLength = 160;
+		// Pick an iteration count that works for you. The NIST recommends at
+		// least 1,000 iterations:
+		// http://csrc.nist.gov/publications/nistpubs/800-132/nist-sp800-132.pdf
+		// iOS 4.x reportedly uses 10,000:
+		// http://blog.crackpassword.com/2010/09/smartphone-forensics-cracking-blackberry-backup-passwords/
+		int iterations = 20000;
+
+		KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, iterations, derivedKeyLength);
+
+		SecretKeyFactory f = SecretKeyFactory.getInstance(algorithm);
+
+		return f.generateSecret(spec).getEncoded();
+	}
+
+	public static byte[] generateSalt() throws NoSuchAlgorithmException {
+		// VERY important to use SecureRandom instead of just Random
+		SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+
+		// Generate a 8 byte (64 bit) salt as recommended by RSA PKCS5
+		byte[] salt = new byte[8];
+		random.nextBytes(salt);
+
+		return salt;
+	}
+}
