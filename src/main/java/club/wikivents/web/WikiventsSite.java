57de9a9ddd594212aa57ea946684394320ab9d5d
Mark Hooijkaas <mark@hooijkaas.net>
2015-08-05 22:35:27 +0200

    cleanup

--- src/main/java/club/wikivents/web/WikiventsSite.java
+++ src/main/java/club/wikivents/web/WikiventsSite.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 import org.kisst.http4j.HttpCall;
 import org.kisst.http4j.HttpCallDispatcher;
+import org.kisst.http4j.HttpCallDispatcher.Path;
 import org.kisst.http4j.HttpCallHandler;
 import org.kisst.http4j.ResourceHandler;
 import org.kisst.item4j.struct.Struct;
@@ -32,13 +33,11 @@ public class WikiventsSite implements HttpCallHandler {
 		public final HttpCallHandler scripts=new ResourceHandler("resources/scripts/", "src/resources/scripts");
 		public final HttpCallHandler js=new ResourceHandler("resources/js/", "src/resources/js");
 		public final HttpCallHandler images=new ResourceHandler("resources/images/", "src/resources/images");
-		@HttpCallDispatcher.Path(dispatchPath="favicon.ico")
+		@Path(dispatchPath="favicon.ico")
 		public final HttpCallHandler favicon =new ResourceHandler("resources/favicon.ico", "src/resources/favicon.ico");
 	}
 	public WikiventsSite(Struct props) {
 		this.model=WikiventsModels.createModel(this, props);
-		//this.engine=new TemplateEngine(props.getStruct("handlebars"));
-		//engine.registerUserHelpers(User.class, "authenticatedUser");
 		Struct themeProps = props.getStruct("theme");
 		for (String name: themeProps.fieldNames())
 			themes.put(name, new WikiventsTheme(themeProps.getStruct(name)));
@@ -46,10 +45,6 @@ public class WikiventsSite implements HttpCallHandler {
 		this.loginPage=new LoginPage(this);
 		this.pages=new Pages();
 		this.handler = new HttpCallDispatcher(pages);
-		//for (User u :model.users) {
-		//	if (u.encryptedPassword==null)
-		//		u.setPassword(u.password);
-		//}
 	}
 	@Override public void handle(HttpCall httpcall, String subPath) {
 		WikiventsCall call = WikiventsCall.of(httpcall, model);
