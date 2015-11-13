cf1af35561cefe56f7bf59ffaebac51ab8426a71
Mark Hooijkaas <mark@hooijkaas.net>
2015-11-13 13:12:49 +0100

    Save old version

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-ant jar ; scp wikivents-0.0.1.jar  wikivents:site ; ssh wikivents sudo service wikivents restart
+ant jar ; ssh wikivents cp site/wikivents-0.0.1.jar wikivents-`date "+%Y-%m-%d-%H.%M.%S"`.jar; scp wikivents-0.0.1.jar  wikivents:site ; ssh wikivents sudo service wikivents restart
