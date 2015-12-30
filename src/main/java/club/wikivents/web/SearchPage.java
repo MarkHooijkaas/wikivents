553ff8a37b8e4b96413b1b49b1ea2fd61950c38f
Mark Hooijkaas <mark@hooijkaas.net>
2015-12-30 10:39:24 +0100

    Added searching in themes

--- src/main/java/club/wikivents/web/SearchPage.java
+++ src/main/java/club/wikivents/web/SearchPage.java
@@ -6,6 +6,7 @@ import org.kisst.http4j.HttpCall;
 import org.kisst.http4j.handlebar.TemplateEngine.TemplateData;
 
 import club.wikivents.model.Event;
+import club.wikivents.model.Group;
 import club.wikivents.model.User;
 
 public class SearchPage extends WikiventsPage {
@@ -21,20 +22,27 @@ public class SearchPage extends WikiventsPage {
 			text=text.toLowerCase();
 		ArrayList<Event> events=new ArrayList<Event>() ;
 		for (Event e: call.model.allEvents) {
-			if ((e.description+e.title+e.city+e.location).toLowerCase().indexOf(text)>0) {
+			if ((e.description+e.title+e.city+e.location).toLowerCase().indexOf(text)>=0) {
 				events.add(e);
+			}
+		}
+		ArrayList<Group> themes=new ArrayList<Group>() ;
+		for (Group g: call.model.groups) {
+			if ((g.description+g.title).toLowerCase().indexOf(text)>=0) {
+				themes.add(g);
 			}
 		}
 		if (call.authenticated) {
 			ArrayList<User> users=new ArrayList<User>() ;
 			for (User u: call.model.users) {
-				if ((u.description+u.username+u.city).toLowerCase().indexOf(text)>0) {
+				if ((u.description+u.username+u.city).toLowerCase().indexOf(text)>=0) {
 					users.add(u);
 				}
 			}		
 			context.add("users", users);
 		}
 		context.add("events", events);
+		context.add("themes", themes);
 		call.output(call.getTheme().search.toString(context));
 	}
 
