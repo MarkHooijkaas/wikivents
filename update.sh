bd5bfa67f4d413224308df91a937c0f2a5a09bcc
Mark Hooijkaas <mark@hooijkaas.net>
2017-03-09 03:50:14 +0100

    fix update script

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-ant jar 
-ssh wikivents cp site/wikivents-0.0.1.jar wikivents-`date "+%Y-%m-%d-%H.%M.%S"`.jar
-scp wikivents-0.0.1.jar  wikivents:site
-ssh wikivents sudo service wikivents restart
+ant jar
+ssh wikivents.nl cp site/wikivents-0.0.1.jar wikivents-`date "+%Y-%m-%d-%H.%M.%S"`.jar
+scp wikivents-0.0.1.jar  wikivents.nl:site
+ssh wikivents.nl sudo service wikivents restart
